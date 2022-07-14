119be61474f4a937accf94f699047eb768d0f156
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-07-14 22:00:33 +0300

    first

---
+++ index.js
@@ -0,0 +1,40 @@
+// var kiosk={
+//     price:30.00,
+//     fruitsname:"oranges",
+//     quantity:2
+// }
+// console.log(kiosk.price* kiosk.quantity)
+
+class fruits{
+    constructor(price){
+        this.price=price
+        this.totalprice=function(fruitsname,quantity){
+            let v=price *quantity;
+            return `${quantity} ${fruitsname} for KES.  ${v} .`;  
+        }
+    }
+}
+var myprice= new fruits(60.00);
+console.log(myprice.totalprice("Oranges",2));
+
+
+// number2
+class kioskcalc{
+    constructor(fruit,quantity){
+        this.fruit=fruit
+        this.quantity=quantity
+        this. fruitPricelist={
+            "orange": 30, 
+            "mango": 15,
+            "avocado": 40 
+        }
+        this.getTotalCost=function(){
+            return `${quantity} ${fruit} for KES ${quantity * this.price}`
+        }
+      
+    }
+}
+kioskcalc.prototype.price = 80;
+var kiosk2=new kioskcalc('mango',2)
+console.log(kiosk2.getTotalCost());
+
